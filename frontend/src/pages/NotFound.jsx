b4194ba8ddf7900d228abaf6a5c4d214b6882764
Mihhail Matvejev <m4tveevm@icloud.com>
2025-02-08 23:30:53 +0300

    feat: creating simple bootstrap page with carousel and big button with link to participate in JouTak

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <Container className="text-center my-5">
+            <h1 className="display-4">404</h1>
+            <p className="lead">Страница не найдена</p>
+            <Button as={Link} to="/" variant="primary">
+                Вернуться на главную
+            </Button>
+        </Container>
+    );
+};
+
+export default NotFound;
---
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+    return (<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sit amet velit nec tristique. Nulla
+            mattis semper sem, sed bibendum mauris posuere et. Pellentesque vitae sem eu neque auctor scelerisque a et
+            quam. Etiam in euismod tellus, eget viverra justo. Morbi turpis urna, porta id nulla at, convallis ornare
+            lorem. Pellentesque malesuada turpis dictum augue volutpat, ut mollis odio pellentesque. Fusce laoreet eu
+            lorem nec pharetra. Proin vel cursus ligula. Aliquam tristique sed neque vitae tristique. Morbi tristique
+            orci quis laoreet gravida. Fusce mollis semper libero, at ultricies dolor suscipit vitae.</p>
+    )
+};
+
+export default PrivacyPolicy;

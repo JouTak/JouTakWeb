1799a7c0eb0042f1c241139335b880b3d4546c2e
Mihhail Matvejev <m4tveevm@icloud.com>
2025-02-12 23:58:24 +0300

    REFACTORING: fixing code style issues adding missed linter import
    Signed-off-by: Mihhail Matvejev <m4tveevm@icloud.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,36 +1,36 @@
-import {Footer} from "@gravity-ui/navigation";
+import { Footer } from "@gravity-ui/navigation";
 
 const CustomFooter = () => {
-    return (
-        <Footer
-            className="page-footer"
-            withDivider={false}
-            moreButtonTitle="Show more"
-            copyright={`@ ${new Date().getFullYear()} "JouTak"`}
-            logo={{
-                // icon: logoIcon,
-                iconSize: 24,
-                text: "JouTak",
-            }}
-            menuItems={[
-                {
-                    text: "Политика конфиденциальности",
-                    href: "/privacy-policy",
-                    target: "blank",
-                },
-                {
-                    text: "Условия использования",
-                    href: "/privacy-policy",
-                    target: "blank",
-                },
-                {
-                    text: "Контакты",
-                    href: "/contact",
-                    target: "blank",
-                },
-            ]}
-        />
-    );
+  return (
+    <Footer
+      className="page-footer"
+      withDivider={false}
+      moreButtonTitle="Show more"
+      copyright={`@ ${new Date().getFullYear()} "JouTak"`}
+      logo={{
+        // icon: logoIcon,
+        iconSize: 24,
+        text: "JouTak",
+      }}
+      menuItems={[
+        {
+          text: "Политика конфиденциальности",
+          href: "/privacy-policy",
+          target: "blank",
+        },
+        {
+          text: "Условия использования",
+          href: "/privacy-policy",
+          target: "blank",
+        },
+        {
+          text: "Контакты",
+          href: "/contact",
+          target: "blank",
+        },
+      ]}
+    />
+  );
 };
 
 export default CustomFooter;

d83b49773dd23bc6996d722c44efdace07fb5383
Denis Berman <secondissa@yandex.ru>
2025-02-25 01:42:34 +0300

    added contact page + loaded font in main(be careful)

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,44 @@
-// const Contact = () => {
-//     return (
-//
-//     );
-// };
-//
-// export default Contact;
+const Contact = () => {
+    return (
+        <div className="p-5 mb-4 rounded-3">
+            
+        <div className="container py-5">
+          <h1 className="display-5 fw-bold">Наши сообщества</h1>
+          <p className="col-md-10 fs-4 lh-1">
+            Подпишись, чтобы быть в курсе новостей Джоутека, ИТМОкрафта и майнкрафта!
+          </p>
+          <div className="d-flex flex-column gap-2">
+            <a
+                className="btn btn-info btn-lg"
+                href="https://t.me/+HHAU5go3GqIzYmI6"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-telegram-plane me-2"></i>
+                Наш ТГ
+            </a>
+            <a
+                className="btn btn-info btn-lg"
+                href="https://vk.com/itmocraft"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-vk me-2"></i>
+                Наш ВК
+            </a>
+            <a
+                className="btn btn-info btn-lg"
+                href="https://discord.gg/YVj5tckahA"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-discord me-2"></i>
+                Дискорд Джоутека
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Contact;

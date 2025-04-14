04c0d16073ec9ed7ff9a14313905dfb155c4e106
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2025-04-14 11:47:39 +0300

    base page

--- frontend/src/pages/Minigames.jsx
+++ frontend/src/pages/Minigames.jsx
@@ -1,83 +1,107 @@
+import "react";
+
 const MiniGames = () => {
   return (
-    <div className="text-center">
-      <div
-        id="carouselExampleInterval"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="10000">
-            <img
-              src="/img/aether-portal.png"
-              className="d-block w-100"
-              alt="Aether Portal"
-            />
-          </div>
-          <div className="carousel-item" data-bs-interval="10000">
-            <img
-              src="/img/final-stage.png"
-              className="d-block w-100"
-              alt="Final Stage"
-            />
-          </div>
-          <div className="carousel-item" data-bs-interval="10000">
-            <img
-              src="/img/top-players.png"
-              className="d-block w-100"
-              alt="top players"
-            />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="prev"
+      <div className="text-center">
+        <div
+            id="carouselExampleInterval"
+            className="carousel slide"
+            data-bs-ride="carousel"
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-
-      <div className="p-5 mb-4 rounded-3">
-        <div className="container py-5">
-          <h1 className="display-5 fw-bold">JouTak</h1>
-          <p className="col-md-8 fs-4 lh-1 mx-auto">
-            Мини-игры — неотъемлемая часть майнкрафта. Сервер JouTak MINIGAMES
-            представляет собой как классические игры (тут будут игры), так и
-            полностью написанные с нуля. Более того, именно здесь проходит
-            спартакиада по майнкрафту!
-          </p>
-          <a
-            className="btn btn-primary btn-lg disabled"
-            href="#"
-            style={{ pointerEvents: "none" }}
-            title="Регистрация еще не началась"
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="10000">
+              <img
+                  src="/img/aether-portal.png"
+                  className="d-block w-100"
+                  alt="Aether Portal"
+              />
+            </div>
+            <div className="carousel-item" data-bs-interval="10000">
+              <img
+                  src="/img/final-stage.png"
+                  className="d-block w-100"
+                  alt="Final Stage"
+              />
+            </div>
+            <div className="carousel-item" data-bs-interval="10000">
+              <img
+                  src="/img/top-players.png"
+                  className="d-block w-100"
+                  alt="Top Players"
+              />
+            </div>
+          </div>
+          <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="prev"
           >
-            Зарегистрироаться на спартакиаду
-          </a>
-          <div className="alert alert-warning mt-3">
-            Регистрация пока еще не началась
+          <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+          ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="next"
+          >
+          <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+          ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+        <div className="container py-3">
+          <div className="my-4">
+            <img
+                src="/img/logoMiniGames.jpg"
+                alt="MiniGames Logo"
+                style={{height: "100px"}}
+            />
           </div>
+          <p className="col-md-8 fs-4 lh-1 mx-auto">
+            Мини-игры стали неотъемлемой частью Minecraft, и наш сервер приглашает вас окунуться в мир
+            соревнований на спартакиаде от{" "}
+            <a href="https://vk.com/kb_esports" target="_blank" rel="noopener noreferrer">
+              Кронверскских барсов
+            </a>.
+            Здесь вас ждут проверенные классические режимы, разработанные с нуля от наших разработчиков:<br></br> <b>Block
+            Party, Ace Race, UHC</b>.
+          </p>
+
+          <div className="my-4 d-flex justify-content-center">
+            <a
+                className="btn btn-secondary btn-lg mx-2"
+                href="https://vk.cc/cDvqOw"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Вступить в беседу
+            </a>
+            <a
+                className="btn btn-primary btn-lg mx-2"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfX7C2f1WII6Ak_me3onbRAcb71MSEap51MS-Hic4XYg915MA/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Зарегистрироваться
+            </a>
+          </div>
+          <a
+              href="https://docs.google.com/document/d/1TasKKNFDkostGTnX0SsSpCzvgKw7tITA/edit?tab=t.0"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Читать регламент спартакиады
+          </a>
         </div>
       </div>
-    </div>
   );
 };
 

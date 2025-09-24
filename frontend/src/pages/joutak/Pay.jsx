7fb4923d6e98b64227334429ad47c488539c2584
mantevian <zmantevian@gmail.com>
2025-09-24 22:20:03 +0300

    fix prettier

--- frontend/src/pages/joutak/Pay.jsx
+++ frontend/src/pages/joutak/Pay.jsx
@@ -1,28 +1,31 @@
-import { Link } from 'react-router-dom';
-import './pay.css';
+import { Link } from "react-router-dom";
+import "./pay.css";
 
 export default function Pay() {
   return (
     <section className="text-center mb-4">
       <h1 className="mt-4 display-5 fw-bold">Оплата проходочки</h1>
-      <p className="mt-4 fs-4 lh-xs mx-auto">Теперь оплатить доступ к&nbsp;JouTak можно на&nbsp;этой странице!</p>
-      <Link
-        className="btn btn-primary btn-lg"
-        to="/joutak/"
-      >
+      <p className="mt-4 fs-4 lh-xs mx-auto">
+        Теперь оплатить доступ к&nbsp;JouTak можно на&nbsp;этой странице!
+      </p>
+      <Link className="btn btn-primary btn-lg" to="/joutak/">
         О сервере
       </Link>
 
       <p className="mt-4 col-md-9 fs-4 lh-xs mx-auto">
         Джоутек спонсируют только его&nbsp;игроки.
         <br />
-        Каждый месяц мы&nbsp;скидываемся на&nbsp;хостинг&nbsp;— никто на&nbsp;этом ничего не&nbsp;зарабатывает, это&nbsp;способ существования сервера.
+        Каждый месяц мы&nbsp;скидываемся на&nbsp;хостинг&nbsp;— никто
+        на&nbsp;этом ничего не&nbsp;зарабатывает, это&nbsp;способ существования
+        сервера.
       </p>
 
       <p className="mt-2 col-md-9 fs-4 lh-xs mx-auto">
-        Всё работает по&nbsp;принципам донейшена: вы&nbsp;оплачиваете любую сумму, но&nbsp;не&nbsp;меньше минимальной.
-        Чем больше (в&nbsp;перерасчёте на&nbsp;месяц) присылают игроки, тем&nbsp;лучше себя чувствует сервер.
-        За&nbsp;дополнительную оплату игроки не&nbsp;получают привилегий.
+        Всё работает по&nbsp;принципам донейшена: вы&nbsp;оплачиваете любую
+        сумму, но&nbsp;не&nbsp;меньше минимальной. Чем больше
+        (в&nbsp;перерасчёте на&nbsp;месяц) присылают игроки, тем&nbsp;лучше себя
+        чувствует сервер. За&nbsp;дополнительную оплату игроки не&nbsp;получают
+        привилегий.
       </p>
 
       <script src="https://forms.yandex.ru/_static/embed.js"></script>

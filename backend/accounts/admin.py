810a849d27e807e4a00325b54e9330f450c11e06
Mihhail Matvejev <matveevmihail@yandex-team.ru>
2026-03-10 22:31:43 +0300

    Address Copilot review edge cases

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import logging
+
 from accounts.services.email_addresses import sync_user_email_address
 from django.contrib import admin
 from django.contrib.admin.sites import NotRegistered
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
@@ -12,7 +16,7 @@ User = get_user_model()
 try:
     admin.site.unregister(User)
 except NotRegistered:
-    pass
+    logger.debug("User admin was not registered before custom registration")
 
 
 @admin.register(User)
@@ -20,4 +24,3 @@ class UserAdmin(DjangoUserAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
         sync_user_email_address(obj)
-

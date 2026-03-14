5b9aeb364054cd5de5ec5e453d9d01405d3b3a66
alena orlova <orlowalesa35@gmail.com>
2026-03-14 18:16:26 +0300

    chore(event-section): implement event section
    feat(faq-section): create new FAQ section

---
+++ frontend/src/components/FAQsection/FAQsection.jsx
@@ -0,0 +1,14 @@
+import QAblock from './QAblock'
+import styles from './faq.module.css'
+import faq from './faq.data.js'
+
+export default function FAQsection() {
+    return (
+        <section className={styles.faqSection}>
+            <h2 className={styles.title}>FAQ</h2>
+            {faq.map(element => (
+                <QAblock key={element.question} {...element}/>
+            ))}
+        </section>
+    )
+}

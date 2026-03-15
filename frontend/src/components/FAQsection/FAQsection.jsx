a4bfd112764ec92b8166142866b54b993b8db7fd
alena orlova <orlowalesa35@gmail.com>
2026-03-15 13:00:37 +0300

    адаптивка до 750px

--- frontend/src/components/FAQsection/FAQsection.jsx
+++ frontend/src/components/FAQsection/FAQsection.jsx
@@ -5,10 +5,12 @@ import faq from './faq.data.js'
 export default function FAQsection() {
     return (
         <section className={styles.faqSection}>
-            <h2 className={styles.title}>FAQ</h2>
-            {faq.map(element => (
-                <QAblock key={element.question} {...element}/>
-            ))}
+            <div className={styles.inner}>
+                <h2 className={styles.title}>FAQ</h2>
+                {faq.map(element => (
+                    <QAblock key={element.question} {...element}/>
+                ))}
+            </div>
         </section>
     )
 }

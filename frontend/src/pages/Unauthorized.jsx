bc927ee20a1f7123021da010fdb503a051d9f8d6
Daniil Klimchuk <l1tedane4ek@gmail.com>
2026-04-07 18:32:38 +0300

    fix(frontend): `<RequireAuth />` now points to 401 feat(frontend): updated styling of `<Unauthorized />` and added theme toggle button

--- frontend/src/pages/Unauthorized.jsx
+++ frontend/src/pages/Unauthorized.jsx
@@ -1,20 +1,47 @@
-import { Container, Button, ButtonGroup } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Button, ThemeProvider } from "@gravity-ui/uikit";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const Unauthorized = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+
+  const reason = params.get("reason");
+  const next = params.get("next");
+
+  const [theme, setTheme] = useState("dark");
+
   return (
-    <Container className="text-center my-5">
-      <h1 className="display-4">401 -- Unauthorized</h1>
-      <p className="lead">Доступно только авторизованным пользователям</p>
-      <ButtonGroup>
-        <Button as={Link} to="/login" variant="primary">
-          Войти
-        </Button>
-        <Button as={Link} to="/" variant="secondary">
-          На главную
-        </Button>
-      </ButtonGroup>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container className="d-flex flex-column align-items-center gap-2 p-2">
+        <h2 className="m-0">401 - Unauthorized</h2>
+        <p className="m-0">
+          {reason === "auth_required"
+            ? "Вы пытаетесь открыть раздел доступный только после входа"
+            : "У вас нет доступа к этой странице"}
+        </p>
+        <div className="d-flex gap-2">
+          <Button
+            as={Link}
+            href={`/login?next=${encodeURIComponent(next)}`}
+            view="action"
+          >
+            Войти
+          </Button>
+          <Button as={Link} href="/">
+            На главную
+          </Button>
+          <Button
+            as={Link}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            Toggle theme
+          </Button>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 };
 

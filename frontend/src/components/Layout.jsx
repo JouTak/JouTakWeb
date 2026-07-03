a5bc0a7ce3377c8bb45a8756265c4141652da4ff
NeMomet <70071099+NeMomet@users.noreply.github.com>
2026-07-03 23:37:29 +0300

    feat(registry): add needed flags to registry
    chore(husky): make husky work in shell
    feat(featureflags): integrate flags to layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,21 @@
 import PropTypes from "prop-types";
+
+import FeatureGate from "../features/featureFlags/FeatureGate";
+import Footer from "../Legacy/frontend/src/components/Footer";
+import FooterNew from "./Footer/Footer";
 import Header from "./Header";
 import HeaderNew from "./HeaderNew/HeaderNew";
-
 
 const Layout = ({ children }) => {
   return (
     <>
-      <HeaderNew />
+      <FeatureGate flag="site_new_header" fallback={<Header />}>
+        <HeaderNew />
+      </FeatureGate>
       <main className="w-100">{children}</main>
-      <Footer />
+      <FeatureGate flag="site_new_footer" fallback={<Footer />}>
+        <FooterNew />
+      </FeatureGate>
     </>
   );
 };

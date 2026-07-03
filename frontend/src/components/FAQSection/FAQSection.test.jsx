3362215b6bac064fb6a8f5a8c6da01aa6dca5a42
NeMomet <70071099+NeMomet@users.noreply.github.com>
2026-07-03 23:48:55 +0300

    fix(faq): deleting file temporarly

--- frontend/src/components/FAQSection/FAQSection.test.jsx
+++ frontend/src/components/FAQSection/FAQSection.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 
-import FAQSection from "./FAQsection.jsx";
+import FAQSection from "./SectionFAQ.jsx";
 
 const items = [
   {

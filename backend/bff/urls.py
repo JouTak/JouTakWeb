de0e6074a86810c1d7cda31ae1b86a1ff73a683b
NeMomet <70071099+NeMomet@users.noreply.github.com>
2026-07-04 17:21:05 +0300

    chore(backend): addition to previous commit

--- backend/bff/urls.py
+++ backend/bff/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("bootstrap", views.bootstrap),
     path("pages/home", views.homepage),
     path("account/summary", views.account_summary),
+    # For feature usage
+    path("pages/minigames", views.minigames),
+    path("pages/itmocraft", views.itmocraft),
 ]

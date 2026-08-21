f41c6c82fb5962636168e27782620357f87b25f2
NeMomet <70071099+NeMomet@users.noreply.github.com>
2026-08-21 15:06:22 +0500

    chore(contact-page): addition to prev. commit

---
+++ frontend/src/components/ContactButton/ContactButton.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+import styles from "./ContactButton.module.scss";
+
+export default function ContactButton({ link, iconPath, label }) {
+  return (
+    <a className={styles.contactButton} href={link}>
+      <span>
+        <img src={iconPath} alt={[label, "ico"].join("-")} />
+      </span>
+      <span>{label}</span>
+    </a>
+  );
+}
+
+ContactButton.propTypes = {
+  link: PropTypes.string.isRequired,
+  iconPath: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
